292bebdf48b3d490a92647b88aad352b5b3beb8e
IamAakash73 <msdaakash73@gmail.com>
2026-03-10 10:08:54 +0530

    Add files via upload

---
+++ Remove duplicated from sorted list.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* current = head;
+        
+        while (current != nullptr && current->next != nullptr) {
+            if (current->val == current->next->val) {
+                ListNode* nextNode = current->next;
+                current->next = current->next->next;
+                delete nextNode; 
+            } else {
+                current = current->next;
+            }
+        }
+        
+        return head;
+    }
+};
